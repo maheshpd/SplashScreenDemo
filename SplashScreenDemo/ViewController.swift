44c0ae557c8023d3eb00f1e078b7e156bf5942c8
maheshprasad <mahesh223prasad@gmail.com>
2018-11-28 15:57:01 +0530

    Initial Commit

---
+++ SplashScreenDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashScreenDemo
+//
+//  Created by Mahesh Prasad on 28/11/18.
+//  Copyright © 2018 CreatesApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
