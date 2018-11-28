8073db1159568dcf3bc9c70bf3c7353fbfc7c266
maheshprasad <mahesh223prasad@gmail.com>
2018-11-28 16:07:40 +0530

    Done Splash Screen

--- SplashScreenDemo/ViewController.swift
+++ SplashScreenDemo/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+3) {
+            self.performSegue(withIdentifier: "nextPage", sender: nil)
+        }
     }
 
 
